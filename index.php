ea2ee8f76a9a76ebd0beb727f3a624b6b6a1563d
robings <r_gordon_stables@hotmail.com>
2020-01-22 10:51:15 +0000

    tested some code to build the deck

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <?php
 //blackjack aces low, picture cards 10
+
+$deckOfCards = [
+    'heart1' => [
+    'face' => 'Ace of Hearts',
+    'score' => 1
+    ],
+    'heart2' => [
+        'face' => 'Two of Hearts',
+        'score' => 2
+    ],
+    'heart3' => [
+        'face' => 'Three of Hearts',
+        'score' => 3
+    ]
+];
+
+$deckOfCards['heart4'] = [ 'face' => 'Four of Hearts', 'score' => 4 ];
+
+var_dump($deckOfCards);
+echo '<br /><br />';
+    shuffle($deckOfCards);
+
+var_dump($deckOfCards);
