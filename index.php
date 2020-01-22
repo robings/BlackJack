432203e96436558e9ccc29e363ed4ac20dba1a98
robings <r_gordon_stables@hotmail.com>
2020-01-22 14:46:36 +0000

    began splitting out html, linked to seperate css file

--- index.php
+++ index.php
@@ -72,21 +72,7 @@ $dealerHandScore = $deckOfCards[1]['score'] + $deckOfCards[3]['score'];
  * @param $dealerScoreParam
  */
 function buildPageDisplay($playerHandParam, $playerScoreParam, $dealerHandParam, $dealerScoreParam) {
-   echo '<!DOCTYPE html>';
-   echo '<html lang="en-GB">';
 
-   echo '<head>';
-    echo '<title>Mayden Academy Casino | Blackjack</title>';
-    echo '<style>';
-
-    echo '</style>';
-
-    echo '</head>';
-
-    echo '<body>';
-
-    echo '<section>';
-    echo '<h1>BlackJack</h1>';
     echo '<div class="player">';
     echo '<h2>Player</h2>';
     echo $playerHandParam[0]['face'] . '<br />';
@@ -104,13 +90,7 @@ function buildPageDisplay($playerHandParam, $playerScoreParam, $dealerHandParam,
      echo whoWins($playerScoreParam , $dealerScoreParam);
     echo '</div>';
 
-    echo '</section>';
 
-    echo '<p>Disclaimer: This is just for practice. Mayden Academy doesn\'t really run a casino.</p>';
-
-    echo '</body>';
-
-    echo '</html>';
 }
 
 buildPageDisplay($playerHand, $playerHandScore, $dealerHand, $dealerHandScore);
@@ -135,3 +115,33 @@ function whoWins($playerScore, $dealerScore) {
 //echo '<br />' . $playerHandScore;
 //echo '<br />' . $dealerHandScore;
 ////
+?>
+
+//html
+<!DOCTYPE html>
+<html lang="en-GB">
+
+<head>
+<title>Mayden Academy Casino | Blackjack</title>
+    <link rel="stylesheet" type="text/css" href="normalize.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+
+
+</head>
+
+<body>
+
+<section>
+<h1>BlackJack</h1>
+    <?php
+
+    ?>
+
+
+ </section>
+
+<p>Disclaimer: This is just for practice. Mayden Academy doesn&#039;t really run a casino.</p>
+
+</body>
+
+</html>
