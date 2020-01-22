0e6e45e24fcfdf111a4ccaf8976bddaa2722193c
robings <r_gordon_stables@hotmail.com>
2020-01-22 11:25:48 +0000

    built card deck

--- index.php
+++ index.php
@@ -4,13 +4,35 @@
 $deckOfCards = [];
 
 //build the deck
-for ($i=1; $i<5; $i++) {
-    for ($j=1; $j<14; $j++) {
-        if ($i==1) {
-            $faceName="Hearts";
-        } elseif ($i==2) {
-            $faceName="Clubs";
+for ($i = 1; $i < 5; $i ++) {
+    for ($j = 1; $j < 14; $j ++) {
+        //determine suit
+        if ($i == 1) {
+            $suitName = 'Hearts';
+        } elseif ($i == 2) {
+            $suitName = 'Clubs';
+        } elseif ($i == 3) {
+            $suitName = 'Diamonds';
+        } elseif ($i == 4) {
+            $suitName = 'Spades';
+        } else {
+            echo 'opps there isn\'t a fifth suit';
         }
+
+        //what if face is a picture card?
+        if ($j > 10) {
+            $face = determinePictureFace ($j);
+            $cardScore = 10;
+        } else {
+            $face = $j;
+            $cardScore = $j;
+        }
+
+        $deckArrayKey = $suitName . $j;
+        $faceName = $face. ' of ' . $suitName;
+
+        $deckOfCards[$deckArrayKey] = ['face' => $faceName, 'score' => $cardScore];
+
     }
 
 
@@ -19,6 +41,15 @@ for ($i=1; $i<5; $i++) {
 
 }
 
+function determinePictureFace ($pictureFaceNo) {
+    if ($pictureFaceNo == 11) {
+        return 'Jack';
+    } elseif ($pictureFaceNo == 12) {
+        return 'Queen';
+    } elseif ($pictureFaceNo == 13) {
+        return 'King';
+    }
+}
 
 
 //$deckOfCards = [
