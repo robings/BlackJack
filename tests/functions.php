2a851cc18580e4562c20cd81766f20a009cd5351
robings <r_gordon_stables@hotmail.com>
2020-01-27 13:46:12 +0000

    added tests functions.php file

---
+++ tests/functions.php
@@ -0,0 +1,32 @@
+<?php
+
+require '../functions.php';
+
+use PHPUnit\Framework\TestCase;
+
+class FunctionTests extends TestCase {
+  public function testdeterminePictureFaceSuccessJack() {
+      $expected = 'Jack';
+      $input = 11;
+      $case = determinePictureFace($input);
+      $this->assertEquals($expected, $case);
+  }
+    public function testdeterminePictureFaceSuccessQueen() {
+        $expected = 'Queen';
+        $input = 12;
+        $case = determinePictureFace($input);
+        $this->assertEquals($expected, $case);
+    }
+    public function testdeterminePictureFaceSuccessKing() {
+        $expected = 'King';
+        $input = 13;
+        $case = determinePictureFace($input);
+        $this->assertEquals($expected, $case);
+    }
+    public function testdeterminePictureFaceFailure() {
+        $expected = NULL;
+        $input = 5;
+        $case = determinePictureFace($input);
+        $this->assertEquals($expected, $case);
+    }
+}
